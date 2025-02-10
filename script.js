d507273252aeba4e6224dabc07ae415c1f4ca340
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-10 10:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 function convertToRoman(num) {
-  const arr = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
+    const arr = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
 
-  let roman = '';
+    let roman = '';
 
-  for (let [symbol, value] of arr) {
-    while (num >= value) {
-      roman += symbol;
-      num -= value;
+    for (let [symbol, value] of arr) {
+        while (num >= value) {
+            roman += symbol;
+            num -= value;
+        }
     }
-  }
 
-  return roman;
+    return roman;
 }
-
-// Example usage:
-console.log(convertToRoman(1987)); // Output: MCMLXXXVII
-
 
 
 
